dfa264750051f89e2301972b6af896cd3672eea9
Dmitri <alchovicdmitri@gmail.com>
2022-02-23 21:31:55 +0300

    added battle page

--- TableTopBattleTracker/ClientApp/src/services/api.ts
+++ TableTopBattleTracker/ClientApp/src/services/api.ts
@@ -4,105 +4,114 @@ import {
   ApiAction,
   ApiSpecialAbility,
   ApiSpellcasting,
+  Action,
+  Ability,
+  SpecialAbility,
+  Damage,
 } from "../interfaces";
 
 class Service {
-    _apiBase = "/"; //https://www.dnd5eapi.co/api/";
+  _apiBase = "https://www.dnd5eapi.co/api";
 
-    async getResource(url: string): Promise<any> {
+  async getResource(url: string) {
     const res: Response = await fetch(`${this._apiBase}${url}`);
 
     if (!res.ok) {
       throw new Error("Could not fetch" + `${res.status}`);
-        }
-        return await res.json();
     }
+
+    return await res.json();
+  }
 
   async getMonstersList() {
     const res = await this.getResource(`/monsters/`);
     return res.results;
   }
   /* async getMonstersList() {
-=======
-     let t = await res.json();
-
- return t; 
-}
-
- async getMonstersList() {
-    return await this.getResource(`monsters/`);
-} 
-/* async getMonstersList() {
->>>>>>> 0377e901591d27bb77b625d0e53b5003c76f2610
     const res= await axios.get(`${this._apiBase}/monsters/`) 
    console.log(res.data.results.results);
     
     return await (res);
 } */
-
   async getMonster(index: string) {
     const monster = await this.getResource(`/monsters/${index}`);
     return this._transfomCharInfo(monster);
   }
 
-  _transfomCharInfo(char: any) {
+  findSpecialAbilities (abilitiesArray:ApiSpecialAbility[]|undefined):SpecialAbility[]|undefined
+  {
+    let specialAbilities = undefined;
+     if (abilitiesArray) {
+       specialAbilities =
+        abilitiesArray.map(
+          (SpecialAbility: ApiSpecialAbility): SpecialAbility => {
+            const { spellcasting, ...newSpecialAbility } = SpecialAbility;
+            if (spellcasting) {
+              const {
+                components_required: componentsRequired,
+                ...spellcastingChanged
+              } = spellcasting;
+              const newSpellcasting = {
+                ...spellcastingChanged,
+                componentsRequired,
+              };
+              return { ...SpecialAbility, spellcasting: newSpellcasting };
+            } else return { ...newSpecialAbility };
+          }
+        );
+    }
+    else {specialAbilities=undefined}
+    
+    return(specialAbilities)
+  }
 
-    const actions = char.actions.map((ApiAction: ApiAction) => {
+  _transfomCharInfo(char: ApiCharacter): Character {
+    const actions = char.actions.map((ApiAction: ApiAction): Action => {
       const damageArr = ApiAction.damage.map(
-        ({ damage_type: damageType, damage_dice: damageDice }) => {
+        ({ damage_type: damageType, damage_dice: damageDice }): Damage => {
           return { damageType, damageDice };
         }
       );
-      const { attack_bonus: attackBonus, ...newAction } = ApiAction;
-      return { ...newAction, attackBonus, damage: damageArr };
-    });
+      if (ApiAction.attack_bonus) {
+        const { attack_bonus: attackBonus, ...newAction } = ApiAction;
 
-    const specialAbilites = char.special_abilities.map(
-      ({ spellcasting, ...ability }: ApiSpecialAbility) => {
-        if (spellcasting) {
-          const {
-            components_required: componentsRequired,
-            ...spellcastingChanged
-          } = spellcasting;
-          const newSpellcasting = {
-            ...spellcastingChanged,
-            componentsRequired,
-          };
-          return { ...ability, spellcasting: newSpellcasting };
-        } else return ability;
-      }
-    );
-    const { senses:sensesChange } = char;
-    const {passive_perception:passivePerception,...newSenses} = sensesChange;
-     const senses = {...newSenses, passivePerception} ;
+        return { ...newAction, attackBonus, damage: damageArr };
+      } else return { ...ApiAction, damage: damageArr };
+    });
+     const specialAbilities = this.findSpecialAbilities(char.special_abilities); 
+
+    const { senses: sensesChange } = char;
+    const { passive_perception: passivePerception, ...newSenses } =
+      sensesChange;
+    const senses = { ...newSenses, passivePerception };
     const {
       armor_class: armorClass,
       challenge_rating: challengeRaiting,
-      condition_immunities: conditionImmunitiesk,
+      condition_immunities: conditionImmunities,
       damage_immunities: damageImmunities,
       damage_resistances: damageResistances,
       damage_vulnerabilities: damageVulnerabilities,
       hit_dice: hitDice,
       hit_points: hitPoints,
-      legendary_actions:legendaryActions,
+      legendary_actions: legendaryActions,
       special_abilities,
       ...newChar
-    } = char;
+    }: ApiCharacter = char;
 
     return {
       ...newChar,
       actions: actions,
       armorClass,
       challengeRaiting,
-      conditionImmunitiesk,
+      conditionImmunities,
       damageImmunities,
       damageResistances,
       damageVulnerabilities,
       hitDice,
       hitPoints,
       legendaryActions,
-      specialAbilites,
-      senses:senses
+      specialAbilities,
+      senses: senses,
     };
   }
 }
